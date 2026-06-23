393ad07f6b042a8bd556e0b73491e2af931cff2a
Kiszeyov <bognarata06@gmail.com>
2026-06-23 20:44:12 +0200

    LAST UPDATE

--- src/main.cpp
+++ src/main.cpp
@@ -67,9 +67,12 @@ float Ta{0.0};                       // band temperature.
 float Tb{0.0};                       // finger temperature.
 float SpO2{0.0};                     // blood oxygen saturation level.
 float Hemoglobin{0.0};               // hemoglobin concentration.
-const float matrix[2][3] = {{-0.0000216869, 0.000597656, 0.000397867}, {0.000330283, -0.0000697138, -0.0000240794}};
+const double matrix[2][3] = {{-0.00002168690296471037, 0.0005976560022420870, 0.0003978668462422693}, {0.0003302831835958265, -0.00006981376124276799, -0.00002407939737095606}};
+const double fmat[1][25] = {-0.006954043447019526, -0.0042377025013406585, -0.0027254709751402028, 0.001925331764954539, 0.010531816191115602, 0.023422586252651936, 0.04027641685412209, 0.05998282263542721, 0.08077845795359356, 0.10043037202633504, 0.11660408714493363, 0.12725084664140338, 0.13096381921182298, 0.12725084664140338, 0.11660408714493363, 0.10043037202633504, 0.08077845795359356, 0.05998282263542721, 0.04027641685412209, 0.023422586252651936, 0.010531816191115602, 0.001925331764954539, -0.0027254709751402028, -0.0042377025013406585, -0.006954043447019526, };
 
 XPT2046_Touchscreen ts(CS_PIN, TIRQ_PIN);
+
+bool run = true;
 
 File data;
 
@@ -77,8 +80,8 @@ hw_timer_t *Timer0_Cfg = NULL;
 
 struct SensorData
 {
-  float AC;
-  float DC;
+  double AC;
+  double DC;
 };
 
 SensorData *SENS660 = nullptr;
@@ -87,7 +90,7 @@ SensorData *SENS940 = nullptr;
 
 
 
-size_t BufferSize = 100;
+size_t BufferSize = 1000;
 size_t Index = 0;
 
 
@@ -98,6 +101,7 @@ void TFTsetup()
   tft.setRotation(3);
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(2);
   Serial.println("TFT up");
   ts.begin();
   ts.setRotation(1);
@@ -139,23 +143,23 @@ void TFTsetup()
 void DPupdate( void * pvParameters){
   Serial.println("Task created and started");
   for (;;){
-    tft.fillScreen(TFT_BLACK);
-    tft.print("band temperature(°C): ");
-    tft.println(Ta);
-    tft.setCursor(0, 10);
-    tft.print("finger temperature(°C): ");
-    tft.println(Tb);
-    tft.setCursor(0, 19);
-    tft.print("SpO2(%): ");
-    tft.println(SpO2);
-    tft.setCursor(0, 38);
-    tft.print("Hemoglobin(g/dl): ");
-    tft.println(Hemoglobin);
-    tft.setCursor(0, 47);
-    tft.println("Tap to end measurement");
-    tft.setCursor(0, 0);
-    vTaskDelay(500 / portTICK_PERIOD_MS);
-
+      tft.fillScreen(TFT_BLACK);
+      tft.print("band temperature(°C): ");
+      tft.println(Ta);
+      tft.setCursor(0, 15);
+      tft.print("finger temp(°C):");
+      tft.println(Tb);
+      tft.setCursor(0, 30);
+      tft.print("SpO2(%): ");
+      tft.println(SpO2);
+      tft.setCursor(0, 45);
+      tft.print("Hemoglobin(g/dl): ");
+      tft.println(Hemoglobin);
+      tft.setCursor(0, 60);
+      tft.println("Tap to end measurement");
+      tft.setCursor(0, 0);
+      vTaskDelay(250 / portTICK_PERIOD_MS);
+    
 
   }
 }
@@ -172,33 +176,61 @@ void Tread()
   Tb = analogRead(ADC2T) * ADCRes; // Convert ADC reading to voltage and then to temperature
 }
 
+void filter(int ind) {
+  /*SENS660[ind].DC = SENS660[ind].DC * fmat[1][0];
+  SENS940[ind].DC = SENS940[ind].DC * fmat[1][0];
+  SENS810[ind].DC = SENS810[ind].DC * fmat[1][0];
+  for (int i {ind-1}; i > (ind-25); i--) {
+    SENS660[ind].DC += SENS660[i].DC * fmat[1][ind - i];
+    SENS940[ind].DC += SENS940[i].DC * fmat[1][ind - i];
+    SENS810[ind].DC += SENS810[i].DC * fmat[1][ind - i];
+  }*/
+
+}
+
 void test(void * pvParameter)
 {
-  for (;;){
+  for (;;) {
       digitalWrite(LED660, HIGH);
       analogWrite(LEDDAC, L660C);
-      vTaskDelay(100 / portTICK_PERIOD_MS);
-      SENS660[Index].AC = analogRead(ADC1Ph);
-      SENS660[Index].DC = analogRead(DAC2);
-      Serial.println(SENS660[Index].DC);
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+      SENS660[Index].AC = (analogRead(ADC1Ph) * ADCRes);
+      SENS660[Index].DC = (analogRead(DAC2) * ADCRes);
+      Serial.print(SENS660[Index].DC);
+      Serial.print(',');
+      Serial.print(SENS660[Index].AC);
+      Serial.print(',');
       digitalWrite(LED660, LOW);
-      analogWrite(LEDDAC, L810C);
-      digitalWrite(LED810, HIGH);
-      vTaskDelay(100 / portTICK_PERIOD_MS);
-      SENS810[Index].AC = analogRead(ADC1Ph);
-      SENS810[Index].DC = analogRead(DAC2);
-      digitalWrite(LED810, LOW);
+     
       analogWrite(LEDDAC, L940C);
       digitalWrite(LED940, HIGH);
-      vTaskDelay(100 / portTICK_PERIOD_MS);
-      SENS940[Index].AC = analogRead(ADC1Ph);
-      SENS940[Index].DC = analogRead(DAC2);
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+      SENS940[Index].AC = (analogRead(ADC1Ph) * ADCRes);
+      SENS940[Index].DC = (analogRead(DAC2) * ADCRes);
+      Serial.print(SENS940[Index].DC);
+      Serial.print(',');
+      Serial.println(SENS940[Index].AC);
       Index++;
       digitalWrite(LED940, LOW);
+      analogWrite(LEDDAC, L810C);
+      digitalWrite(LED810, HIGH);
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+      SENS810[Index].AC = (analogRead(ADC1Ph) * ADCRes);
+      SENS810[Index].DC = (analogRead(DAC2) * ADCRes);
+      Serial.print(SENS810[Index].DC);
+      Serial.print(',');
+      Serial.print(SENS810[Index].AC);
+      Serial.print(',');
+      digitalWrite(LED810, LOW);
+      //vTaskDelay(10 / portTICK_PERIOD_MS);
+
     
   }
 }
 
+
+
+const double convert {-1.61134386078};
 
 void HBcalc(float R660, float R810, float R940)
 {
@@ -207,15 +239,17 @@ float Hemo1;float Hemo2;
 Hemo1 = (R660 * matrix[1][1])+(R940 * matrix[1][2])+(R810 * matrix[1][3]);
 Hemo2 = (R660 * matrix[2][1])+(R940 * matrix[2][2])+(R810 * matrix[2][3]);
 
-Hemoglobin =  (Hemo1+Hemo2)*1000;
+Hemoglobin =  (Hemo1+Hemo2)*1000 * convert;
 
 }
 
 void calc()
 {
-  int Lindex = Index - 6;
+  int Lindex = Index - 25;
   if (Lindex > 0)
   {
+
+    filter(Lindex);
     float R660 = abs(SENS660[Lindex].AC / (SENS660[Lindex].DC - 1.6));
     float R810 = abs(SENS810[Lindex].AC / (SENS810[Lindex].DC - 1.6));
     float R940 = abs(SENS940[Lindex].AC / (SENS940[Lindex].DC - 1.6));
@@ -277,8 +311,12 @@ void setup()
 }
 
 
+long tslast {0};
+
 void loop()
 {
-    Tread();
-    calc();
+
+      Tread();
+      calc();
+    
 }
